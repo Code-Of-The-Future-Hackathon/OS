15fe0dd05671c1f7ddccd463896cccad93d370ef
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-12-08 21:31:40 +0200

    Add router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,5 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <router-view></router-view>
 </template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,4 @@
+
+<template>
+    <h1>Home</h1>    
+</template>
