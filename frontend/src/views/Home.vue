e93db4e5e59cd2abd4f6416b5bf80a9e65c125e7
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-12-09 11:55:35 +0200

    Change colors

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,4 +1,62 @@
 
 <template>
-    <h1>Home</h1>    
+    <div class="h-screen flex flex-col justify-center items-center bg-black">
+        <div class="flex items-center flex-col gap-4 text-white">
+            <h1 class="font-bold text-8xl">OS</h1>
+            <p class="w-1/2 text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, nobis blanditiis voluptate, laborum suscipit debitis aperiam, earum dicta vel beatae rerum autem laboriosam possimus amet.</p>
+        </div>
+    </div>
+    <div class="bg-[#1E1E1E] flex flex-col items-center relative">
+       <div class="bg-[#A3F68E] w-1/2 h-1/3 rounded-[30px] border-2 border-[#1E1E1E] mt-36 p-10 justify-center items-center">
+            <div class="flex">
+                <h1 class="text-5xl font-bold w-full break-words">NATURAL DISASTER PREDICTION MAPS</h1>
+
+                <div class="flex items-center justify-center flex-col text-center w-full gap-4">
+                    <p>Real time based natural disaster prediction maps. Choose a disaster by layers</p>
+                    <button class="bg-[#E9E3DC] px-6 font-bold shadow-sm shadow-black py-1 border-2 border-[#1E1E1E]">MAPS</button>
+                </div>
+            </div>
+       </div> 
+
+       <h1 class="font-bold text-4xl text-white my-32">OUT IDEA</h1>
+
+       <div class="bg-[#00B085] w-2/5 h-[80vh] rounded-[30px] flex justify-center items-center">      
+            <div class="bg-[#A3F68E] w-[97%] h-[97%] rounded-[25px] flex flex-col items-center font-bold text-white text-6xl pt-14 text-center">
+                <h1>Natural disaster</h1>
+            </div>
+       </div>
+    </div>
+
+    <div class="h-screen bg-[#1E1E1E] flex items-center justify-center w-screen">
+        <div class="w-1/2">
+            <div class="flex flex-col gap-5">
+                <h1 class="text-white text-6xl text-bold">Advantages</h1>
+                <p class="text-white text-xl"> What are the advantages of our product? </p>
+            </div>
+
+            <div class="flex h-[70vh] gap-4 mt-16">
+                <div class="flex h-full gap-4 flex-col w-full">
+                    <div class="h-2/3 w-full bg-[#A3F68E] rounded-[40px]">
+
+                    </div>
+                    <div class="h-1/3 w-full bg-[#00B085] rounded-[40px]">
+
+                    </div>
+                </div>
+                <div class="flex h-full gap-4 flex-col w-full">
+                    <div class="h-1/3 w-full bg-[#00B085] rounded-[40px]">
+
+                    </div>
+
+                    <div class="h-2/3 w-full bg-[#A3F68E] rounded-[40px]">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+
+</script>
